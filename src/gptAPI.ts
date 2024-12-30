956d334af2341248caaad5ea5b64acc3d648d164
Jangmyun <cndlsrb2739@gmail.com>
2024-12-30 11:01:58 +0900

    setapikey and model

--- src/gptAPI.ts
+++ src/gptAPI.ts
@@ -11,10 +11,37 @@ export const setApiKey: vscode.Disposable = vscode.commands.registerCommand("ope
     password: true,
   });
 
+  // apiKey 입력받았을 시 user setting에 gpt api 키 등록
   if (apiKey) {
     await vscode.workspace.getConfiguration().update("openAI.apiKey", apiKey, vscode.ConfigurationTarget.Global);
     vscode.window.showInformationMessage("Open AI API key saved successfully.");
   } else {
     vscode.window.showInformationMessage("No API key entered.");
+  }
+});
+
+/**
+ * @param model Open AI model alias
+ */
+async function configureModelSelected(model: string): Promise<void> {
+  await vscode.workspace.getConfiguration().update("openAI.modelSelected", model, vscode.ConfigurationTarget.Global);
+  vscode.window.showInformationMessage(`GPT model (${model}) selected successfully.`);
+}
+
+export const setGPTModel: vscode.Disposable = vscode.commands.registerCommand("openAI.setModel", async () => {
+  const gptModel = await vscode.window.showInformationMessage(
+    "Select a Open AI GPT model",
+    {
+      detail: `A large model like gpt-4o will offer a very high level of intelligence and strong performance, while having a higher cost per token.
+    A small model like gpt-4o-mini offers intelligence not quite on the level of the larger model, but is faster and less expensive per token.
+    A reasoning model like the o1 family of models is slower to return a result, and uses more tokens to "think", but is capable of advanced reasoning, coding, and multi-step planning.`,
+      modal: true,
+    },
+    "gpt-4o",
+    "gpt-4o-mini",
+    "o1"
+  );
+  if (gptModel) {
+    configureModelSelected(gptModel);
   }
 });
